a7b539190809aecb99133a038b099b87dfc9d80c
Jair Deyner <jairdeyner730791@gmail.com>
2023-01-09 10:06:10 -0500

    JsonPipeComponent modified

--- src/app/components/json-pipe/json-pipe.component.ts
+++ src/app/components/json-pipe/json-pipe.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+interface Person {
+  Apellido: string;
+  Casado: boolean;
+  Direccion: string;
+  Nombre: string;
+  Telefono: number;
+}
+
 @Component({
   selector: 'app-json-pipe',
   templateUrl: './json-pipe.component.html',
@@ -7,5 +15,20 @@ import { Component } from '@angular/core';
   ]
 })
 export class JsonPipeComponent {
-
+  persons: Array<Person> = [
+    {
+      Apellido: 'Tello',
+      Casado: false,
+      Direccion: 'Lima -Perú',
+      Nombre: 'Jair',
+      Telefono: 987654322,
+    },
+    {
+      Apellido: 'Romeo',
+      Casado: true,
+      Direccion: 'Lima -Perú',
+      Nombre: 'Jorge',
+      Telefono: 765432546,
+    },
+  ]
 }
